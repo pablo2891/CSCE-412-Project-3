8205b306bb5285fd16698556c06516bf2271895a
Pablo Pineda <pablopineda@tamu.edu>
2024-06-16 23:46:37 -0500

    ran doxy for the last time

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['web_5fserver_2ecpp_0',['web_server.cpp',['../web__server_8cpp.html',1,'']]]
+  ['request_2ecpp_0',['request.cpp',['../request_8cpp.html',1,'']]],
+  ['request_5fqueue_2ecpp_1',['request_queue.cpp',['../request__queue_8cpp.html',1,'']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "lrw",
-  1: "lrw"
+  0: "lmrw",
+  1: "lmrw",
+  2: "m"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files"
+  1: "files",
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files"
+  1: "Files",
+  2: "Functions"
 };
 
